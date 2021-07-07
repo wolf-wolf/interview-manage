751c334f6cba81a99c96e018ce43580d047a7952
Matthew Wang <wangmaozhu@foxmail.com>
2021-07-08 00:02:11 +0800

    feat: 添加自定义顶部导航栏组件

---
+++ miniprogram/components/top-nav-bar/index.js
@@ -0,0 +1,32 @@
+// components/top-nav-bar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    showBack: {
+      type: Boolean,
+    },
+    title: {
+      type: String,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    statusBarHeight: wx.getSystemInfoSync()['statusBarHeight'],
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    goBack: function() {
+      wx.navigateBack({
+        delta: 0,
+      })
+    }
+  }
+})
